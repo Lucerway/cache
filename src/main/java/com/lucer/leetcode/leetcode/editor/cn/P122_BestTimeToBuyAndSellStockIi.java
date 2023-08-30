c90b8ecae43d87e651c25bbdb2855fee193a85a6
liuchuan <liuchuan@yupaopao.cn>
2023-08-30 19:23:59 +0800

    p121 p122买股票

--- src/main/java/com/lucer/leetcode/leetcode/editor/cn/P122_BestTimeToBuyAndSellStockIi.java
+++ src/main/java/com/lucer/leetcode/leetcode/editor/cn/P122_BestTimeToBuyAndSellStockIi.java
@@ -54,7 +54,8 @@ public class P122_BestTimeToBuyAndSellStockIi {
     public static void main(String[] args) {
         Solution solution = new P122_BestTimeToBuyAndSellStockIi().new Solution();
 
-        System.out.println();
+        int[] n = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(solution.maxProfit(n));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
@@ -64,6 +65,57 @@ public class P122_BestTimeToBuyAndSellStockIi {
      */
     class Solution {
         public int maxProfit(int[] prices) {
+            if (prices.length < 2) {
+                return 0;
+            }
+
+            // 定义状态、状态变化方程、初始值、确定值
+            int length = prices.length;
+            int[][] dp = new int[length][2];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+
+            for (int i = 1; i < length; i++) {
+                // 其中0代表持有现金，1代表持有股票
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[length - 1][0];
+        }
+    }
+
+
+//leetcode submit region end(Prohibit modification and deletion)
+
+    class SolutionB {
+
+        /**
+         * 通用动态规划
+         */
+        public int maxProfit(int[] prices) {
+
+            if (prices.length < 2) {
+                return 0;
+            }
+
+            // 定义状态、状态变化方程、初始值、确定值，其中0代表持有现金，1代表持有股票
+            int length = prices.length;
+            int[][] dp = new int[length][2];
+            dp[0][0] = 0;
+            dp[0][1] = -prices[0];
+
+            for (int i = 1; i < length; i++) {
+                dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][i] + prices[i]);
+                dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
+            }
+            return dp[length - 1][0];
+        }
+
+
+        /**
+         * 贪心算法：拿到每一段可以拿到的收益！
+         */
+        public int maxProfit2(int[] prices) {
             int maxProfit = 0;
             for (int i = 1; i < prices.length; i++) {
                 if (prices[i] > prices[i - 1]) {
@@ -73,6 +125,5 @@ public class P122_BestTimeToBuyAndSellStockIi {
             return maxProfit;
         }
     }
-//leetcode submit region end(Prohibit modification and deletion)
 
 }
